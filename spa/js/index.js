334afbad9ca3974e307f2ada724c9d1aad3d0692
Guy Orazem <wesorazem@gmail.com>
2022-04-21 11:00:32 -0600

    added new carpet pages to sw and index.js

--- spa/js/index.js
+++ spa/js/index.js
@@ -2,10 +2,10 @@
 console.log("SPA JS loaded");
 // importing main pages
 import dashboard from "/spa/js/views/dashboard.js";
+import upholstery from "/spa/js/views/upholstery.js";
 import carpet from "/spa/js/views/carpet.js";
 import hardSurface from "/spa/js/views/hard-surface.js";
 // importing upholstery pages
-import upholstery from "/spa/js/views/upholstery.js";
 import upholsteryAdhesives from "/spa/js/views/upholstery-adhesives.js";
 import upholsteryBleach from "/spa/js/views/upholstery-bleach.js";
 import upholsteryCompound from "/spa/js/views/upholstery-compound.js";
@@ -23,6 +23,25 @@ import upholsterySoils from "/spa/js/views/upholstery-soils.js";
 import upholsteryUrine from "/spa/js/views/upholstery-urine.js";
 // end importing upholstery pages
 // importing carpet pages
+import carpetAdhesives from "/spa/js/views/carpet-adhesives.js";
+import carpetBleach from "/spa/js/views/carpet-bleach.js";
+import carpetClay from "/spa/js/views/carpet-clay.js";
+import carpetCompound from "/spa/js/views/carpet-compound.js";
+import carpetCurry from "/spa/js/views/carpet-curry.js";
+import carpetFiltration from "/spa/js/views/carpet-filtration.js";
+import carpetFurniture from "/spa/js/views/carpet-furniture.js";
+import carpetPolish from "/spa/js/views/carpet-polish.js";
+import carpetInks from "/spa/js/views/carpet-inks.js";
+import carpetOils from "/spa/js/views/carpet-oils.js";
+import carpetOrganic from "/spa/js/views/carpet-organic.js";
+import carpetPaint from "/spa/js/views/carpet-paint.js";
+import carpetPh from "/spa/js/views/carpet-ph.js";
+import carpetProtein from "/spa/js/views/carpet-protein.js";
+import carpetRust from "/spa/js/views/carpet-rust.js";
+import carpetSynthetic from "/spa/js/views/carpet-synthetic.js";
+import carpetToner from "/spa/js/views/carpet-toner.js";
+import carpetSalt from "/spa/js/views/carpet-salt.js";
+import carpetUrine from "/spa/js/views/carpet-urine.js"; 
 
 const navigateTo = url =>  {
     history.pushState(null, null, url);
@@ -54,7 +73,28 @@ const router = async () => {
         { path: "/upholstery-urine", view: upholsteryUrine},
         // end importing upholstery pages
         // importing carpet pages
-        
+        { path: "/carpet-adhesives", view: carpetAdhesives},
+        { path: "/carpet-bleach", view: carpetBleach},
+        { path: "/carpet-compound", view: carpetCompound},
+        { path: "/carpet-curry", view: carpetCurry},
+        { path: "/carpet-clay", view: carpetClay},
+        { path: "/carpet-filtration", view: carpetFiltration},
+        { path: "/carpet-furniture", view: carpetFurniture},
+        { path: "/carpet-polish", view: carpetPolish},
+        { path: "/carpet-inks", view: carpetInks},
+        { path: "/carpet-oils", view: carpetOils},
+        { path: "/carpet-organic", view: carpetOrganic},
+        { path: "/carpet-paint", view: carpetPaint},
+        { path: "/carpet-ph", view: carpetPh},
+        { path: "/carpet-protein", view: carpetProtein},
+        { path: "/carpet-rust", view: carpetRust},
+        { path: "/carpet-salt", view: carpetSalt},
+        { path: "/carpet-synthetic", view: carpetSynthetic},        
+        { path: "/carpet-urine", view: carpetUrine},
+        { path: "/carpet-toner", view: carpetToner},
+
+
+          // end importing carpet pages
       
     ];
 
